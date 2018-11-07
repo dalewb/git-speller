88587cf6bdcd1ade1d890298302abaf555623910
William Dale <williambdale@gmail.com>
2018-11-06 20:36:19 -0500

    Define grid area

---
+++ index.js
@@ -0,0 +1,3 @@
+document.querySelectorAll('[transform="translate(16, 20)"]')[0].children[0].children[0].setAttribute("fill", "black")
+
+const grid = [...document.querySelectorAll('[transform="translate(16, 20)"]')[0].children].slice(0,52)
