e393f0680a1facf52933f390663cf23af35a1422
William Dale <williambdale@gmail.com>
2018-11-06 20:43:12 -0500

    Create makeRow function from grid

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 document.querySelectorAll('[transform="translate(16, 20)"]')[0].children[0].children[0].setAttribute("fill", "black")
 
 const grid = [...document.querySelectorAll('[transform="translate(16, 20)"]')[0].children].slice(0,52)
+
+
+const getBoxes = (row) => {
+  
+}
+
+const makeRow = (grid) => {
+  grid.forEach((row) => {
+    console.log(row.children)
+  })
+}
