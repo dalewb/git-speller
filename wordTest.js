aed4badf3f4e2332d4ee74ce8e70531b18207b03
William Dale <williambdale@gmail.com>
2018-11-07 00:56:21 -0500

    Prelim word test

--- wordTest.js
+++ wordTest.js
@@ -2,11 +2,53 @@
 const makeWord = (word) => {
   const grid = [...document.querySelectorAll('[transform="translate(16, 20)"]')[0].children].slice(0,52)
   const letters = word.split('')
-  letters.forEach(letter => {
-    makeLetter(letter)
-  })
+  for (k = 0; k < letters.length; k++) {
+    // conditional for having too long of a string to fit in boxes
+    let startingX = (13 - (k * 4))
+    let xIndex = (k * 4)
+    for (i = xIndex; i < (xIndex + 4); i++) {
+      getBoxes(grid, letters[i].toLowerCase(), startingX)
+    }
+  }
 }
 
-const makeLetter = (letter) => {
+const getBoxes = (column, letter, startingX) => {
+  let boxes = column.children
+  for (j = 0; j < boxes.length; j++) {
+    makeLetter(boxes[j], letter, startingX)
+  }
+}
 
+const makeLetter = (box, letter, startingX) => {
+  switch (letter) {
+    case "a":
+      makeA(box, letter, startingX)
+      break
+    // case "b":
+    //   makeB(box, letter)
+    //   break
+  }
+}
+
+const makeA = (box, letter, startingX) => {
+  const x = box.attributes[3].value
+  const y = box.attributes[4].value
+  if (y === "0" && (x === startingX || x === (startingX - 1).toString() || x === (startingX - 2).toString())) {
+    box.setAttribute("fill", "#FF69B4")
+  } else if (y === "12" && (x === startingX || (startingX - 2).toString())) {
+    box.setAttribute("fill", "#FF69B4")
+  } else if (y === "24" && (x === startingX || (startingX - 2).toString())) {
+    box.setAttribute("fill", "#FF69B4")
+  } else if (y === "36" && (x === startingX || (startingX - 2).toString())) {
+    box.setAttribute("fill", "#FF69B4")
+  } else if (y === "48" && (x === startingX || x === (startingX - 1).toString() || x === (startingX - 2).toString())) {
+    box.setAttribute("fill", "#FF69B4")
+  } else if (y === "60" && (x === startingX || x === (startingX - 2).toString())) {
+    box.setAttribute("fill", "#FF69B4")
+  } else if (y === "72" && (x === startingX || x === (startingX - 2).toString())) {
+    box.setAttribute("fill", "#FF69B4")
+  }
+  else {
+    box.setAttribute("fill", "#ebedf0")
+  }
 }
