53c7469c4d71b1ea53c1a2829c64891f8d286927
William Dale <williambdale@gmail.com>
2018-11-06 20:50:45 -0500

    Iterate through row collection correctly

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-document.querySelectorAll('[transform="translate(16, 20)"]')[0].children[0].children[0].setAttribute("fill", "black")
-
 const grid = [...document.querySelectorAll('[transform="translate(16, 20)"]')[0].children].slice(0,52)
 
-
 const getBoxes = (row) => {
-  
+  console.log(row)
 }
 
 const makeRow = (grid) => {
-  grid.forEach((row) => {
-    console.log(row.children)
-  })
+  for (i=0; i<grid.length; i++) {
+    getBoxes(grid[i])
+  }
 }
+
+makeRow(grid)
